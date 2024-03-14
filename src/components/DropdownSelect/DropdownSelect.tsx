2cda9f0504383b5489d3b2e6d8b45ce506e365c0
SzDabrowsky <szdabrowsky@gmail.com>
2024-03-14 18:23:42 +0100

    feat: dropdown select

--- src/components/DropdownSelect/DropdownSelect.tsx
+++ src/components/DropdownSelect/DropdownSelect.tsx
@@ -4,25 +4,58 @@ import ArrowIcon from "../ArrowIcon/ArrowIcon";
 import { useState } from "react";
 
 export const DropdownSelect = () => {
-  const [isActive, setIsActive] = useState(false);
+	const [isActive, setIsActive] = useState(false);
+	const [isSelected, setIsSelected] = useState(false);
 
-  return (
-    <div className={`${styles.selectMenu} ${isActive ? styles.active : ""}`}>
-      <div
-        className={styles.selectBtn}
-        onClick={() => {
-          setIsActive(!isActive);
-        }}
-      >
-        <span className={styles.btnText}>Options</span>
-        <ArrowIcon isPressed={isActive} />
-      </div>
+	return (
+		<div className={`${styles.selectMenu} ${isActive ? styles.active : ""}`}>
+			<span className={styles.btnLabel}>Options</span>
+			<div
+				className={styles.selectBtn}
+				onClick={() => {
+					setIsActive(!isActive);
+				}}
+				onFocus={() => {
+					setIsActive(!isActive);
+				}}
+			>
+				{" "}
+				{!isSelected ? (
+					<span className={styles.placeholder}>Select option</span>
+				) : (
+					<span className={styles.optionsValue}>Option</span>
+				)}
+				<ArrowIcon isPressed={isActive} />
+			</div>
 
-      <ul className={styles.options}>
-        <li className={styles.option}>
-          <span></span>
-        </li>
-      </ul>
-    </div>
-  );
+			<ul className={styles.options}>
+				<li
+					className={styles.option}
+					onClick={() => {
+						setIsSelected(true);
+					}}
+				>
+					<span>Opcja 1</span>
+				</li>
+
+				<li
+					className={styles.option}
+					onClick={() => {
+						setIsSelected(true);
+					}}
+				>
+					<span>Opcja 2</span>
+				</li>
+
+				<li
+					className={styles.option}
+					onClick={() => {
+						setIsSelected(true);
+					}}
+				>
+					<span>Opcja 3</span>
+				</li>
+			</ul>
+		</div>
+	);
 };
