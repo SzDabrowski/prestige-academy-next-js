23bf999fa75826134dc183b7cd9fcd5be2d270a3
SzDabrowski <szdabrowsky@gmail.com>
2024-04-08 19:37:57 +0200

    feat: fixed some css and added animation for displaying additional input

--- src/components/landing-page/ContactForm/ContactForm.tsx
+++ src/components/landing-page/ContactForm/ContactForm.tsx
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from "react";
 
 import { Container } from "@/components/Container/Container";
 import { DropdownSelect } from "@/components/DropdownSelect/DropdownSelect";
-import { useForm, SubmitHandler, useFieldArray } from "react-hook-form";
+import { useForm, SubmitHandler } from "react-hook-form";
 
 import { checkIfCourseForPairs } from "@/utils/clientUtils";
 
@@ -14,6 +14,7 @@ import styles from "./ContactFrom.module.scss";
 interface FormInputs {
   selectedDanceCourse: string;
   name: string;
+  pairName?: string;
   email: string;
   phone: string;
 }
@@ -57,37 +58,27 @@ const ContactForm = () => {
       <div className={styles.wrapper}>
         <div className={styles.formContainer}>
           <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
-            {/* <input
+            <div className={styles.inputsContainer}>
+              {/* <input
               type="hidden"
               name="access_key"
               value="7369090a-7acb-46e8-b84d-69101f7fe01a"
             /> */}
 
-            <DropdownSelect
-              title={"Kursy tańca"}
-              options={courseTitles}
-              placeholder={"Wybierz kurs"}
-              getValue={handleDropdownSelect}
-            />
-            <input
-              type="hidden"
-              {...register("selectedDanceCourse")}
-              placeholder={selectedDanceCourse}
-            />
-
-            <label className={styles.label}>
-              <span>Twoje nazwisko</span>
-              <input
-                type="text"
-                {...register("name", { required: true })}
-                placeholder="Jan Kowalski"
+              <DropdownSelect
+                title={"Kurs tańca"}
+                options={courseTitles}
+                placeholder={"Wybierz kurs"}
+                getValue={handleDropdownSelect}
               />
-              {errors.name && <span>This field is required</span>}
-            </label>
+              <input
+                type="hidden"
+                {...register("selectedDanceCourse")}
+                placeholder={selectedDanceCourse}
+              />
 
-            {showDancePartnerInput ? (
               <label className={styles.label}>
-                <span>Nazwisko Partnera:</span>
+                <span>Imię i nazwisko</span>
                 <input
                   type="text"
                   {...register("name", { required: true })}
@@ -95,29 +86,41 @@ const ContactForm = () => {
                 />
                 {errors.name && <span>This field is required</span>}
               </label>
-            ) : (
-              ""
-            )}
 
-            <label className={styles.label}>
-              <span>Adres email</span>
-              <input
-                type="email"
-                {...register("email", { required: true })}
-                placeholder="Twój adres email"
-              />
-              {errors.email && <span>This field is required</span>}
-            </label>
+              <label
+                className={`${styles.label} ${
+                  showDancePartnerInput ? "" : styles.hidden
+                }`}
+              >
+                <span>Partner:</span>
+                <input
+                  type={showDancePartnerInput ? "text" : "hidden"}
+                  {...register("pairName", { required: true })}
+                  placeholder="Anna Kowalska"
+                />
+                {errors.name && <span>This field is required</span>}
+              </label>
 
-            <label className={styles.label}>
-              <span>Numer telefonu</span>
-              <input
-                type="tel"
-                {...register("phone", { required: true })}
-                placeholder="Numer telefonu"
-              />
-              {errors.phone && <span>This field is required</span>}
-            </label>
+              <label className={styles.label}>
+                <span>Adres email</span>
+                <input
+                  type="email"
+                  {...register("email", { required: true })}
+                  placeholder="Twój adres email"
+                />
+                {errors.email && <span>This field is required</span>}
+              </label>
+
+              <label className={styles.label}>
+                <span>Numer telefonu</span>
+                <input
+                  type="tel"
+                  {...register("phone", { required: true })}
+                  placeholder="Numer telefonu"
+                />
+                {errors.phone && <span>This field is required</span>}
+              </label>
+            </div>
 
             <div className="h-captcha" data-captcha="true"></div>
             <button className={styles.button} type="submit">
