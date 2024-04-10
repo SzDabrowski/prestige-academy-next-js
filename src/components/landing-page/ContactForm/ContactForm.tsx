d83bccd61ecab125f46e8f062eacaefbdb844265
SzDabrowsky <szdabrowsky@gmail.com>
2024-04-10 16:27:55 +0200

    feat: styles for errors

--- src/components/landing-page/ContactForm/ContactForm.tsx
+++ src/components/landing-page/ContactForm/ContactForm.tsx
@@ -12,125 +12,144 @@ import danceCourses from "@/data/danceCourses.json";
 import styles from "./ContactFrom.module.scss";
 
 interface FormInputs {
-  selectedDanceCourse: string;
-  name: string;
-  pairName?: string;
-  email: string;
-  phone: string;
+	selectedDanceCourse: string;
+	name: string;
+	pairName?: string;
+	email: string;
+	phone: string;
 }
 
 const ContactForm = () => {
-  const [selectedDanceCourse, setselectedDanceCourse] = useState("");
-  const [showDancePartnerInput, setShowDancePartnerInput] = useState(false);
+	const [selectedDanceCourse, setselectedDanceCourse] = useState("");
+	const [showDancePartnerInput, setShowDancePartnerInput] = useState(false);
 
-  const handleDropdownSelect = (value: string) => {
-    setselectedDanceCourse(value);
-    return value;
-  };
+	const handleDropdownSelect = (value: string) => {
+		setselectedDanceCourse(value);
+		return value;
+	};
 
-  useEffect(() => {
-    console.log("selected use effect: " + selectedDanceCourse);
+	useEffect(() => {
+		console.log("selected use effect: " + selectedDanceCourse);
 
-    //set value of  hook-form field
-    setValue("selectedDanceCourse", selectedDanceCourse);
+		//set value of  hook-form field
+		setValue("selectedDanceCourse", selectedDanceCourse);
 
-    setShowDancePartnerInput(
-      checkIfCourseForPairs(danceCourses, selectedDanceCourse)
-    );
-  }, [selectedDanceCourse]);
+		setShowDancePartnerInput(
+			checkIfCourseForPairs(danceCourses, selectedDanceCourse)
+		);
+	}, [selectedDanceCourse]);
 
-  const {
-    register,
-    handleSubmit,
-    setValue,
-    formState: { errors },
-  } = useForm<FormInputs>();
+	const {
+		register,
+		handleSubmit,
+		setValue,
+		formState: { errors },
+	} = useForm<FormInputs>();
 
-  const courseTitles = danceCourses.map((course) => course.title);
+	const courseTitles = danceCourses.map((course) => course.title);
 
-  const onSubmit = (data: FormInputs) => {
-    console.log(data);
-    // Submit your form data here
-  };
+	const onSubmit = (data: FormInputs) => {
+		console.log(data);
+		// Submit your form data here
+	};
 
-  return (
-    <Container>
-      <div className={styles.wrapper}>
-        <div className={styles.formContainer}>
-          <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
-            <div className={styles.inputsContainer}>
-              {/* <input
+	return (
+		<Container>
+			<div className={styles.wrapper}>
+				<div className={styles.formContainer}>
+					<form
+						className={styles.form}
+						onSubmit={handleSubmit(onSubmit)}
+					>
+						<div className={styles.inputsContainer}>
+							{/* <input
               type="hidden"
               name="access_key"
               value="7369090a-7acb-46e8-b84d-69101f7fe01a"
             /> */}
 
-              <DropdownSelect
-                title={"Kurs tańca"}
-                options={courseTitles}
-                placeholder={"Wybierz kurs"}
-                getValue={handleDropdownSelect}
-              />
-              <input
-                type="hidden"
-                {...register("selectedDanceCourse")}
-                placeholder={selectedDanceCourse}
-              />
+							<DropdownSelect
+								title={"Kurs tańca"}
+								options={courseTitles}
+								placeholder={"Wybierz kurs"}
+								getValue={handleDropdownSelect}
+							/>
+							<input
+								type="hidden"
+								{...register("selectedDanceCourse")}
+								placeholder={selectedDanceCourse}
+							/>
 
-              <label className={styles.label}>
-                <span>Imię i nazwisko</span>
-                <input
-                  type="text"
-                  {...register("name", { required: true })}
-                  placeholder="Jan Kowalski"
-                />
-                {errors.name && <span>This field is required</span>}
-              </label>
+							<label className={styles.label}>
+								<span>Imię i nazwisko</span>
+								<input
+									id="name"
+									type="text"
+									{...register("name", { required: true , minLength: {
+                    value: 3,
+                    message: "Recipe name must be at least 3 characters long",
+                  },})}
+									placeholder="Jan Kowalski"
+								/>
+								{errors.name && (
+									<span className={styles.error}>To pole jest wymagane</span>
+								)}
+							</label>
 
-              <label
-                className={`${styles.label} ${
-                  showDancePartnerInput ? "" : styles.hidden
-                }`}
-              >
-                <span>Partner:</span>
-                <input
-                  type={showDancePartnerInput ? "text" : "hidden"}
-                  {...register("pairName", { required: true })}
-                  placeholder="Anna Kowalska"
-                />
-                {errors.name && <span>This field is required</span>}
-              </label>
+							<label
+								className={`${styles.label} ${
+									showDancePartnerInput ? "" : styles.hidden
+								}`}
+							>
+								<span>Partner:</span>
+								<input
+									id="pairName"
+									type={showDancePartnerInput ? "text" : "hidden"}
+									{...register("pairName", { required: true })}
+									placeholder="Anna Kowalska"
+								/>
+								{errors.name && (
+									<span className={styles.error}>To pole jest wymagane</span>
+								)}
+							</label>
 
-              <label className={styles.label}>
-                <span>Adres email</span>
-                <input
-                  type="email"
-                  {...register("email", { required: true })}
-                  placeholder="Twój adres email"
-                />
-                {errors.email && <span>This field is required</span>}
-              </label>
+							<label className={styles.label}>
+								<span>Adres email</span>
+								<input
+									id="email"
+									type="email"
+									{...register("email", { required: true })}
+									placeholder="Twój adres email"
+								/>
+								{errors.name && (
+									<span className={styles.error}>To pole jest wymagane</span>
+								)}
+							</label>
 
-              <label className={styles.label}>
-                <span>Numer telefonu</span>
-                <input
-                  type="tel"
-                  {...register("phone", { required: true })}
-                  placeholder="Numer telefonu"
-                />
-                {errors.phone && <span>This field is required</span>}
-              </label>
-            </div>
+							<label className={styles.label}>
+								<span>Numer telefonu</span>
+								<input
+									id="tel"
+									type="tel"
+									{...register("phone", { required: true })}
+									placeholder="Numer telefonu"
+								/>
+								{errors.name && (
+									<span className={styles.error}>To pole jest wymagane</span>
+								)}
+							</label>
+						</div>
 
-            <div className="h-captcha" data-captcha="true"></div>
-            <button className={styles.button} type="submit">
-              Zapisz się!
-            </button>
-          </form>
-        </div>
-      </div>
-    </Container>
-  );
+						<input
+							className={styles.button}
+							type="submit"
+							value="Zapisz się!"
+						/>
+					</form>
+				</div>
+			</div>
+		</Container>
+	);
 };
 
 export default ContactForm;
