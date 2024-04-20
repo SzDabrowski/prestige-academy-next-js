5d89dae06ea922c4368c93c96ba896e128fce1d9
SzDabrowsky <szdabrowsky@gmail.com>
2024-04-20 12:48:22 +0200

    feat: added data for each instructor, added photos, updated styles for about us page

--- src/app/o_nas/(components)/AboutUsSection/AboutUs.tsx
+++ src/app/o_nas/(components)/AboutUsSection/AboutUs.tsx
@@ -1,47 +1,51 @@
 import styles from "./AboutUs.module.scss";
 import { Container } from "@/components/Container/Container";
 
+import aboutUsData from "@/data/aboutUs.json";
+
 const AboutUs = () => {
-  return (
-    <Container>
-      <section className={styles.section}>
-        <div className={styles.textContent}>
-          <span>O nas i</span>
-          <h3>O naszej akademii tańca</h3>
-          <p>
-            Założycielami Akademii Tańca Prestige, są szeroko utytułowani
-            tancerze Krystian i Greta Kisielewscy, którzy specjalizują się w
-            tańcach latynoamerykańskich jak i standardowych.
-          </p>
-        </div>
-        <div className={styles.flexGroup}>
-          <div className={styles.person}>
-            <div className={styles.avatar}></div>
-            <span className={styles.name}>Greta</span>
-            <span className={styles.info}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit
-            </span>
-          </div>
+	return (
+		<Container>
+			<section className={styles.section}>
+				<div className={styles.textContent}>
+					<span>O nas i</span>
+					<h3>O naszej akademii tańca</h3>
+					<p>{aboutUsData.summary.desc_1}</p>
+					<p>{aboutUsData.summary.desc_2}</p>
+					<div className={styles.quote}>
+						<p>"{aboutUsData.summary.quote}"</p>
+						<p className={styles.signed}>{aboutUsData.summary.quote_sign}</p>
+					</div>
+					<div className={styles.card}></div>
+				</div>
+				<div className={styles.flexGroup}>
+					<div className={styles.person}>
+						<div className={`${styles.avatar} ${styles.krystian}`}></div>
+						<div className={styles.personText}>
+							<span className={styles.name}>Krystian</span>
+							<span className={styles.info}>{aboutUsData.krystian.desc}</span>
+						</div>
+					</div>
 
-          <div className={styles.person}>
-            <div className={styles.avatar}></div>
-            <span className={styles.name}>Krystian</span>
-            <span className={styles.info}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit
-            </span>
-          </div>
+					<div className={styles.person}>
+						<div className={`${styles.avatar} ${styles.greta}`}></div>
+						<div className={styles.personText}>
+							<span className={styles.name}>Greta</span>
+							<span className={styles.info}>{aboutUsData.Greta.desc}</span>
+						</div>
+					</div>
 
-          <div className={styles.person}>
-            <div className={styles.avatar}></div>
-            <span className={styles.name}>Szymon</span>
-            <span className={styles.info}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit
-            </span>
-          </div>
-        </div>
-      </section>
-    </Container>
-  );
+					<div className={styles.person}>
+						<div className={`${styles.avatar} ${styles.szymon}`}></div>
+						<div className={styles.personText}>
+							<span className={styles.name}>Szymon</span>
+							<span className={styles.info}>{aboutUsData.Szymon.desc}</span>
+						</div>
+					</div>
+				</div>
+			</section>
+		</Container>
+	);
 };
 
 export default AboutUs;
