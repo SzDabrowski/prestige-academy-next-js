dbe29da56460c15515c6c74a9a78d46fe5177420
SzDabrowski <szdabrowsky@gmail.com>
2024-04-23 19:33:23 +0200

    feat: updated styles for about us page

--- src/app/o_nas/(components)/AboutUsSection/AboutUs.tsx
+++ src/app/o_nas/(components)/AboutUsSection/AboutUs.tsx
@@ -1,51 +1,121 @@
+"use client";
 import styles from "./AboutUs.module.scss";
 import { Container } from "@/components/Container/Container";
-
 import aboutUsData from "@/data/aboutUs.json";
+import { useState } from "react";
+
+import ArrowIcon from "@/components/icons/ArrowIcon/ArrowIcon";
+
+interface InfoClickedState {
+  greta: boolean;
+  krystian: boolean;
+  szymon: boolean;
+}
 
 const AboutUs = () => {
-	return (
-		<Container>
-			<section className={styles.section}>
-				<div className={styles.textContent}>
-					<span>O nas i</span>
-					<h3>O naszej akademii tańca</h3>
-					<p>{aboutUsData.summary.desc_1}</p>
-					<p>{aboutUsData.summary.desc_2}</p>
-					<div className={styles.quote}>
-						<p>{aboutUsData.summary.quote}</p>
-						<p className={styles.signed}>{aboutUsData.summary.quote_sign}</p>
-					</div>
-					<div className={styles.card}></div>
-				</div>
-				<div className={styles.flexGroup}>
-					<div className={styles.person}>
-						<div className={`${styles.avatar} ${styles.krystian}`}></div>
-						<div className={styles.personText}>
-							<span className={styles.name}>Krystian</span>
-							<span className={styles.info}>{aboutUsData.krystian.desc}</span>
-						</div>
-					</div>
+  const [infoClicked, setInfoClicked] = useState<InfoClickedState>({
+    greta: false,
+    krystian: false,
+    szymon: false,
+  });
 
-					<div className={styles.person}>
-						<div className={`${styles.avatar} ${styles.greta}`}></div>
-						<div className={styles.personText}>
-							<span className={styles.name}>Greta</span>
-							<span className={styles.info}>{aboutUsData.Greta.desc}</span>
-						</div>
-					</div>
+  const handleInfoClick = (person: keyof InfoClickedState) => {
+    setInfoClicked((prevState) => ({
+      ...prevState,
+      [person]: !prevState[person],
+    }));
+  };
 
-					<div className={styles.person}>
-						<div className={`${styles.avatar} ${styles.szymon}`}></div>
-						<div className={styles.personText}>
-							<span className={styles.name}>Szymon</span>
-							<span className={styles.info}>{aboutUsData.Szymon.desc}</span>
-						</div>
-					</div>
-				</div>
-			</section>
-		</Container>
-	);
+  return (
+    <Container>
+      <section className={styles.section}>
+        <div className={styles.textContent}>
+          <span>O nas i</span>
+          <h3>O naszej akademii tańca</h3>
+          <p>{aboutUsData.summary.desc_1}</p>
+          <p>{aboutUsData.summary.desc_2}</p>
+          <div className={styles.quote}>
+            <p>{aboutUsData.summary.quote}</p>
+            <p className={styles.signed}>{aboutUsData.summary.quote_sign}</p>
+          </div>
+          <div className={styles.card}></div>
+        </div>
+        <div className={styles.flexGroup}>
+          <div className={styles.person}>
+            <div className={`${styles.avatar} ${styles.krystian}`}></div>
+            <div className={styles.personText}>
+              <span className={styles.name}>Krystian</span>
+              <div className={styles.infoWrapper}>
+                <div className={styles.info}>
+                  <span
+                    className={`${styles.infoText} ${
+                      infoClicked.krystian ? "" : styles.infoClosed
+                    }`}
+                  >
+                    {aboutUsData.krystian.desc}
+                  </span>
+                  <div
+                    className={styles.infoButton}
+                    onClick={() => handleInfoClick("krystian")}
+                  >
+                    <ArrowIcon isPressed={infoClicked.krystian} />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className={styles.person}>
+            <div className={`${styles.avatar} ${styles.greta}`}></div>
+            <div className={styles.personText}>
+              <span className={styles.name}>Greta</span>
+              <div className={styles.infoWrapper}>
+                <div className={styles.info}>
+                  <span
+                    className={`${styles.infoText} ${
+                      infoClicked.greta ? "" : styles.infoClosed
+                    }`}
+                  >
+                    {aboutUsData.Greta.desc}
+                  </span>
+                  <div
+                    className={styles.infoButton}
+                    onClick={() => handleInfoClick("greta")}
+                  >
+                    <ArrowIcon isPressed={infoClicked.greta} />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className={styles.person}>
+            <div className={`${styles.avatar} ${styles.szymon}`}></div>
+            <div className={styles.personText}>
+              <span className={styles.name}>Szymon</span>
+              <div className={styles.infoWrapper}>
+                <div className={styles.info}>
+                  <span
+                    className={`${styles.infoText} ${
+                      infoClicked.szymon ? "" : styles.infoClosed
+                    }`}
+                  >
+                    {aboutUsData.Szymon.desc}
+                  </span>
+                  <div
+                    className={styles.infoButton}
+                    onClick={() => handleInfoClick("szymon")}
+                  >
+                    <ArrowIcon isPressed={infoClicked.szymon} />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </Container>
+  );
 };
 
 export default AboutUs;
