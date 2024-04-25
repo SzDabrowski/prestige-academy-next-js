e6ce4f2231585e43d8ec6269a1ed00b808442503
SzDabrowsky <szdabrowsky@gmail.com>
2024-04-25 18:15:42 +0200

    fix: lack of key prop in map

--- src/app/umowy/Table/Table.tsx
+++ src/app/umowy/Table/Table.tsx
@@ -4,24 +4,24 @@ import styles from "./Table.module.scss";
 import preschoolsData from "@/data/preschools.json";
 
 export const Table = () => {
-  return (
-    <table className={styles.table}>
-      <thead>
-        <tr>
-          <th>Nazwa przedszkola:</th>
-          <th>Umowa:</th>
-        </tr>
-      </thead>
-      <tbody>
-        {preschoolsData.map((item, index) => {
-          return (
-            <tr>
-              <td>{item.value}</td>
-              <td>Pobierz</td>
-            </tr>
-          );
-        })}
-      </tbody>
-    </table>
-  );
+	return (
+		<table className={styles.table}>
+			<thead>
+				<tr>
+					<th>Nazwa przedszkola:</th>
+					<th>Umowa:</th>
+				</tr>
+			</thead>
+			<tbody>
+				{preschoolsData.map((item, index) => {
+					return (
+						<tr key={index}>
+							<td>{item.value}</td>
+							<td>Pobierz</td>
+						</tr>
+					);
+				})}
+			</tbody>
+		</table>
+	);
 };
