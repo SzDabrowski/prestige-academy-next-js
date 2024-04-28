d1839bc5bb0f6b75afb6d2a35a28f195c851c680
SzDabrowsky <szdabrowsky@gmail.com>
2024-04-28 20:49:09 +0200

    feat: added downloading files to table website

--- src/lib/enums.ts
+++ src/lib/enums.ts
@@ -1,7 +1,12 @@
 export const enum courseForEnum {
-  adults = "dorosli",
-  kids = "dzieci",
-  firstDance = "pierwszy_taniec",
-  includes = "includes",
-  commissioned = "grupy_zlecone",
+	adults = "dorosli",
+	kids = "dzieci",
+	firstDance = "pierwszy_taniec",
+	includes = "includes",
+	commissioned = "grupy_zlecone",
+}
+
+export const enum fileNames {
+	test = "123.txt",
+	pm = "umowa_uczestniczenia_w_zajeciach.pdf",
 }
