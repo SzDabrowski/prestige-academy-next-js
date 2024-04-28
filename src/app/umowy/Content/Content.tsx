45674be83bce75da9cd0d62a5f6537404794cc4e
SzDabrowsky <szdabrowsky@gmail.com>
2024-04-28 20:34:18 +0200

    feat: updated styles and added background image form table page

--- src/app/umowy/Content/Content.tsx
+++ src/app/umowy/Content/Content.tsx
@@ -3,14 +3,16 @@ import styles from "./Content.module.scss";
 import { Table } from "../Table/Table";
 
 export const Content = () => {
-  return (
-    <main className={styles.main}>
-      <section className={styles.titleSection}>
-        <h1>Umowy dla dzieci oraz uczestników zajęć:</h1>
-      </section>
-      <section className={styles.tableSection}>
-        <Table />
-      </section>
-    </main>
-  );
+	return (
+		<div className="">
+			<section className={styles.titleSection}>
+				<h1>Umowy dla dzieci oraz uczestników zajęć:</h1>
+			</section>
+			<main className={styles.main}>
+				<section className={styles.tableSection}>
+					<Table />
+				</section>
+			</main>
+		</div>
+	);
 };
