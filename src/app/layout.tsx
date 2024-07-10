3eb22a2f273fb768b8431fab8d09ebd0767f5455
SzDabrowski <szdabrowsky@gmail.com>
2024-07-10 17:46:26 +0200

    feat: added meta pixel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,8 @@ import { Inter } from "next/font/google";
 import "./globals.scss";
 import Footer from "@/components/Footer/Footer";
 import ChangeHeader from "./ChangeHeader";
+import Script from "next/script";
+import { NextScript } from "next/document";
 
 export default function RootLayout({
   children,
@@ -13,8 +15,34 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pl">
+      <Script
+        id="fb-pixel"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: ` !function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '1968926770230087');
+fbq('track', 'PageView');
+`,
+        }}
+      />
       <head>
         <title>Prestige</title>
+
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src={`https://www.facebook.com/tr?id=1968926770230087&ev=PageView&noscript=1`}
+          />
+        </noscript>
       </head>
       <body>
         <ChangeHeader />
