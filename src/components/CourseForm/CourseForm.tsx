958f418522c17a2d03014aeec823b6fa490c0b60
SzDabrowski <szdabrowsky@gmail.com>
2024-08-01 18:57:05 +0200

    Feat: added rect-hot-toast and added toast messages for form submiting

--- src/components/CourseForm/CourseForm.tsx
+++ src/components/CourseForm/CourseForm.tsx
@@ -12,6 +12,9 @@ import danceCourses from "@/data/danceCourses.json";
 import styles from "./CourseForm.module.scss";
 import ReCAPTCHA from "react-google-recaptcha";
 
+import toast, { Toaster } from "react-hot-toast";
+import { TOAST_MESSAGE } from "@/lib/toastMessages";
+
 interface FormInputs {
   selectedDanceCourse: string;
   name: string;
@@ -81,33 +84,50 @@ const CourseForm = (props: iCourseForm) => {
 
   const onSubmit = async (data: FormInputs, event?: any) => {
     event?.preventDefault();
-    console.log(data);
-    await fetch("https://api.web3forms.com/submit", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Accept: "application/json",
-      },
-      body: JSON.stringify(data, null, 2),
-    })
-      .then(async (response) => {
-        let json = await response.json();
-        if (json.success) {
-          setIsSuccess(true);
-          setMessage(json.message);
-          event.target.reset();
-          reset();
-          setPhoneNumber("");
-        } else {
-          setIsSuccess(false);
-          setMessage(json.message);
-        }
+    const eventPromise = toast.promise(
+      fetch("https://api.web3forms.com/submit", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: JSON.stringify(data, null, 2),
       })
-      .catch((error) => {
-        setIsSuccess(false);
-        setMessage("Client Error. Please check the console.log for more info");
-        console.log(error);
-      });
+        .then(async (response) => {
+          let json = await response.json();
+          if (json.success) {
+            setIsSuccess(true);
+            setMessage(json.message);
+            event.target.reset();
+            reset();
+            setPhoneNumber("");
+            return json.message;
+          } else {
+            setIsSuccess(false);
+            setMessage(json.message);
+            throw new Error(json.message);
+          }
+        })
+        .catch((error) => {
+          setIsSuccess(false);
+          setMessage(
+            "Client Error. Please check the console.log for more info"
+          );
+          console.log(error);
+          throw error;
+        }),
+      {
+        loading: TOAST_MESSAGE.LOADING,
+        success: TOAST_MESSAGE.SUCCESS,
+        error: TOAST_MESSAGE.ERROR,
+      }
+    );
+
+    try {
+      await eventPromise;
+    } catch (error) {
+      console.error("Submission error:", error);
+    }
   };
 
   return (
@@ -257,6 +277,7 @@ const CourseForm = (props: iCourseForm) => {
             // disabled={!capVal}
           />
         </form>
+        <Toaster position="bottom-center" />
       </div>
     </div>
   );
