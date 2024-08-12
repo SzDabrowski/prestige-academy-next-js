f9bda5bea4846dcfab40f18bb4dd5f850d93b6be
SzDabrowski <szdabrowsky@gmail.com>
2024-08-12 21:03:28 +0200

    feat: added banner for course images and updated images

--- src/utils/coursePhotoMapper.ts
+++ src/utils/coursePhotoMapper.ts
@@ -1,7 +1,8 @@
-import clipDance from "../../public/assets/images/courses/clip.jpg";
+import clipDance from "../../public/assets/images/courses/VideoKlipDance.jpg";
 import bachata from "../../public/assets/images/courses/bachataSolo.jpg";
 import bachataPair from "../../public/assets/images/courses/bachata.jpg";
-import kids from "../../public/assets/images/courses/kids.jpg";
+import kids4 from "../../public/assets/images/courses/KidsJunior.jpg";
+import kids7 from "../../public/assets/images/courses/PrestigeJunior7.jpg";
 import kidsPair from "../../public/assets/images/courses/towarzyskiDzieci.jpg";
 import latinoSolo from "../../public/assets/images/courses/latinoSolo.jpg";
 import salsaSolo from "../../public/assets/images/courses/salsaSolo.jpg";
@@ -25,9 +26,9 @@ const mapCourseToPhoto = (title: string) => {
     case "Taniec Towarzyski dla dzieci":
       return kidsPair;
     case "Prestige Junior 5-7 lat":
-      return kids;
+      return kids4;
     case "Prestige Kids 3-4 lata":
-      return kids;
+      return kids7;
 
     case "Video Klip 8 - 11 lat":
       return clipDance;
