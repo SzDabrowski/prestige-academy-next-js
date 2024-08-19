cb2a23b857ebb119a7d496f1df2bc99c5f4cba23
SzDabrowski <szdabrowsky@gmail.com>
2024-08-19 20:53:03 +0200

    fix: fixed email validation in form

--- src/components/CourseForm/CourseForm.tsx
+++ src/components/CourseForm/CourseForm.tsx
@@ -234,7 +234,7 @@ const CourseForm = (props: iCourseForm) => {
                 {...register("email", {
                   required: "To pole jest wymagane",
                   pattern: {
-                    value: /^[a-z0-9.-_]+@[a-z0-9.-]+\.[a-z]{2,4}$/,
+                    value: /^[a-z0-9._-]+@[a-z0-9.-]+\.[a-z]{2,4}$/,
                     message: "Wprowadź poprawny adres email",
                   },
                 })}
