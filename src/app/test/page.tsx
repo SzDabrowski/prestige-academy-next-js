8211f5eac443df3815a00082956b3f997dd33a5f
SzDabrowski <szdabrowsky@gmail.com>
2024-09-12 19:56:45 +0200

    feat: data fetching for courses groups

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,43 +1,21 @@
-// app/page.tsx (or app/home/page.tsx depending on your structure)
-import { getStaticPropsUtil } from "@/lib/action"; // Assuming this fetches your data
-import { getAllGrupyZaj, getGrupyZajById } from "@/lib/contentful/api";
-import { stringify } from "querystring";
-import { ContentfulEntry, getContent } from "@/lib/contentful/client";
-
-interface PostProps {
-  posts: ContentfulEntry[];
-}
-
-const Posts = ({ posts }: PostProps) => {
-  return (
-    <div>
-      {posts.map((post) => (
-        <div key={post.sys.id}>
-          <h2>{post.fields.title}</h2>
-          {post.fields.image && (
-            <img
-              src={post.fields.image.fields.file.url}
-              alt={post.fields.image.fields.title}
-            />
-          )}
-          <p>
-            {post.fields.description?.content
-              .flatMap((p: { content: any[] }) => p.content.map((c) => c.value))
-              .join(" ")}
-          </p>
-        </div>
-      ))}
-    </div>
-  );
-};
+import { fetchCoursesData } from "@/lib/contentful/serverActions/coursesGroups";
+import { draftMode } from "next/headers";
 
 // Server Component
 export default async function Home() {
-  const posts = await getContent("grupyZaj");
+  const blogPosts = await fetchCoursesData({ preview: draftMode().isEnabled });
+  console.log(blogPosts);
 
   return (
     <main>
-      <Posts posts={posts} />
+      {blogPosts.map((blogPost, index) => {
+        return (
+          <li key={blogPost.title}>
+            <h1>{index}</h1>
+            <p>{JSON.stringify(blogPost.description)}</p>
+          </li>
+        );
+      })}
     </main>
   );
 }
