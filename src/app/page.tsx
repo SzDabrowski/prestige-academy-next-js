3e322700a85c48a70d76caf21ca0c98fba7a644f
SzDabrowski <szdabrowsky@gmail.com>
2026-03-22 22:54:52 +0100

    feat: disabled event form and banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,12 +6,10 @@ import CoursesSection from "@/components/landing-page/CoursesSection/CoursesSect
 import AboutUsSection from "@/components/landing-page/AboutUsSection/AboutUsSection";
 import { Container } from "@/components/Container/Container";
 import CourseForm from "@/components/CourseForm/CourseForm";
-import { DanceBanner } from "@/components/DanceEventBanner/DanceEventBanner";
-
 /**
  * Render the landing page by composing primary sections in their display order.
  *
- * @returns A JSX element representing the main landing page element containing the header, hero, course form, dance banner, courses section, and about us section.
+ * @returns A JSX element representing the main landing page element containing the header, hero, course form, courses section, and about us section.
  */
 export default function Home() {
   return (
@@ -21,7 +19,6 @@ export default function Home() {
       <Container>
         <CourseForm />
       </Container>
-      <DanceBanner />
       <CoursesSection />
       <AboutUsSection />
     </main>
